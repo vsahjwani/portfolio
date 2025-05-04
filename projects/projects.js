ece0372d3fa593e4ce4681eaf13db1f93ed79012
vsahjwani <148197257+vsahjwani@users.noreply.github.com>
2025-05-04 00:39:36 -0700

    lowkey lost

--- projects/projects.js
+++ projects/projects.js
@@ -15,50 +15,47 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
     console.error('Error loading projects:', error);
   }
 })();
-
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
 const projects = await fetchJSON('../lib/projects.json');
-let rolledData = d3.rollups(
-  projects,
-  (v) => v.length,
-  (d) => d.year,
-);
 
-let data = rolledData.map(([year, count]) => {
-  return { value: count, label: year };
-});
+function renderPieChart(projectsGiven) {
 
-let colors = d3.scaleOrdinal(d3.schemeTableau10);
-let sliceGenerator = d3.pie().value((d) => d.value);
-let arcData = sliceGenerator(data);
-let arcs = arcData.map((d) => arcGenerator(d));
+  let newRolledData = d3.rollups(
+    projectsGiven,
+    (v) => v.length,
+    (d) => d.year,
+  );
 
-arcs.forEach((arc, idx) => {
-  d3.select('svg')
-    .append('path')
-    .attr('d', arc)
-    .attr('fill',colors(idx))
-})
-
-let legend = d3.select('.legend');
-data.forEach((d, idx) => {
-  legend
-    .append('li')
-    .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
-    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
-});
-
-let query = '';
-let searchInput = document.querySelector('.searchBar');
-searchInput.addEventListener('change', (event) => {
-  // update query value
-  query = event.target.value;
-  // filter projects
-  let filteredProjects = projects.filter((project) => {
-    let values = Object.values(project).join('\n').toLowerCase();
-    return values.includes(query.toLowerCase());
+  let newData = newRolledData.map(([year, count]) => {
+    return { value: count, label: year };
   });
-  // render filtered projects
+
+  let newSliceGenerator = d3.pie().value((d) => d.value);;
+  let newArcData = newSliceGenerator(newData);
+  let newArcs = newArcData.map((d) => arcGenerator(d));
+  newArcs.forEach((arc, idx) => {
+    d3.select('svg')
+      .append('path')
+      .attr('d', arc)
+      .attr('fill',colors(idx))
+  })
+  
+  let legend = d3.select('.legend');
+  data.forEach((d, idx) => {
+    legend
+      .append('li')
+      .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+      .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+  });
+}
+
+
+renderPieChart(projects);
+
+searchInput.addEventListener('change', (event) => {
+  let filteredProjects = setQuery(event.target.value);
   renderProjects(filteredProjects, projectsContainer, 'h2');
+  renderPieChart(filteredProjects);
 });
