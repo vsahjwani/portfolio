f705a75779a49d8b841d63880ae2f800fc3a1274
vsahjwani <148197257+vsahjwani@users.noreply.github.com>
2025-05-03 23:39:15 -0700

    trying

--- projects/projects.js
+++ projects/projects.js
@@ -17,37 +17,16 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 })();
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let arc = arcGenerator({
-  startAngle: 0,
-  endAngle: 2 * Math.PI,
-});
 
-let data = [1, 2];
-let total = 0;
-
-for (let d of data) {
-  total += d;
-}
-
-let angle = 0;
-let arcData = [];
-
-for (let d of data) {
-  let endAngle = angle + (d / total) * 2 * Math.PI;
-  arcData.push({ startAngle: angle, endAngle });
-  angle = endAngle;
-}
-
+let data = [1, 2, 3, 4, 5, 5];
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 
-arcs.forEach((arc) => {
-  d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
-});
-
-let colors = ['gold', 'purple'];
 arcs.forEach((arc, idx) => {
   d3.select('svg')
     .append('path')
     .attr('d', arc)
-    .attr('fill',colors[idx])
+    .attr('fill',colors(idx))
 })
