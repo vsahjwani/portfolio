72ab8a539d70f9b274ec96f9b5a2f0ffa0986c93
vsahjwani <148197257+vsahjwani@users.noreply.github.com>
2025-05-03 23:55:17 -0700

    till step 4 i believe

--- projects/projects.js
+++ projects/projects.js
@@ -18,14 +18,16 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
+const projects = await fetchJSON('../lib/projects.json');
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year,
+);
+
+let data = rolledData.map(([year, count]) => {
+  return { value: count, label: year };
+});
 
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 let sliceGenerator = d3.pie().value((d) => d.value);
@@ -39,3 +41,24 @@ arcs.forEach((arc, idx) => {
     .attr('fill',colors(idx))
 })
 
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+});
+
+let query = '';
+let searchInput = document.querySelector('.searchBar');
+searchInput.addEventListener('change', (event) => {
+  // update query value
+  query = event.target.value;
+  // filter projects
+  let filteredProjects = projects.filter((project) => {
+    let values = Object.values(project).join('\n').toLowerCase();
+    return values.includes(query.toLowerCase());
+  });
+  // render filtered projects
+  renderProjects(filteredProjects, projectsContainer, 'h2');
+});
