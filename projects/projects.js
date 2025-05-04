6434d2fd9789503e901c717351e109f4010ee875
vsahjwani <148197257+vsahjwani@users.noreply.github.com>
2025-05-03 23:42:41 -0700

    idekkkkkkk

--- projects/projects.js
+++ projects/projects.js
@@ -18,9 +18,17 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
+
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 
@@ -30,3 +38,4 @@ arcs.forEach((arc, idx) => {
     .attr('d', arc)
     .attr('fill',colors(idx))
 })
+
